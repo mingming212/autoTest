dac9d3ae981ebdb5c2a308d8154dc1b1c56d12a6
whs <454918603@qq.com>
2019-04-02 11:55:33 +0800

    1

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,9 +4,6 @@ import page.LoginPage;
 import page.MainPage;
 import page.ProfilePage;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
 /**
  * Created by helena.liu on 2019/3/11.
  */
@@ -17,9 +14,14 @@ public class LoginTest {
         MainPage mainPage=MainPage.start();
         ProfilePage profilePage=mainPage.gotoProfile();
         LoginPage loginPage=profilePage.gotoLogin();
+        loginPage.passwordFail("12345678","1234");
 
-        loginPage.passwordFail("12345678","123411");
-        assertThat(loginPage.getMessage(),equalTo("手机号码填写错误"));
+
+//        loginPage.password?("xxxx","xxx");
+//        assertThat(toast,equalTo("手机号码填写错误"));
+
+
+
 
 
     }
