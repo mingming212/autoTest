ffc47ace56b62131ad2f248d52bf8168d137285e
whs <454918603@qq.com>
2019-04-12 18:03:00 +0800

    1

--- src/test/java/justTest/testAppium.java
+++ src/test/java/justTest/testAppium.java
@@ -2,6 +2,7 @@ package justTest;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.TapOptions;
 import io.appium.java_client.touch.offset.ElementOption;
 import io.appium.java_client.touch.offset.PointOption;
 import org.junit.jupiter.api.AfterAll;
@@ -17,7 +18,7 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by helena.liu on 2019/3/11.
+ * Created by whs on 2019/3/11.
  */
 public class testAppium {
     static AppiumDriver driver;
@@ -32,10 +33,11 @@ public class testAppium {
         capabilities.setCapability("noReset","true");
         URL remoteUrl = new URL("http://localhost:4723/wd/hub");
         driver=new AppiumDriver(remoteUrl,capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
-    public void findEle() throws InterruptedException {
+    public void test_getAttribute() throws InterruptedException {
 //        Thread.sleep(3000);
         try {
             String resourceId=driver.findElement(By.id("com.xueqiu.android:id/tab_name")).getAttribute("resourceId");
@@ -46,7 +48,8 @@ public class testAppium {
         System.out.println("222222: ");
     }
 
-    public void longPress(){//长按
+    @Test
+    public void test_longPress(){//长按
         WebElement e=  driver.findElement(By.id("portfolio_stockName"));
         TouchAction touch=new TouchAction(driver);
         touch.longPress(ElementOption.element(e));
@@ -55,15 +58,35 @@ public class testAppium {
     }
 
     @Test
-    public void getLocat(){
-
+    public void test_getLocation() throws InterruptedException {
         WebElement e=  driver.findElement(By.id("com.xueqiu.android:id/tv_search"));
         Point point=e.getLocation();
         System.out.println("1----"+point.toString());
+        e.click();
+        driver.navigate().back();
+        driver.navigate().back();
+
         TouchAction touch =new TouchAction(driver);
-        PointOption pointOption=new PointOption();
-        touch.press(PointOption.point(353,105));
-//        touch.tap(1,2);
+        touch.press(PointOption.point(353,105)).release().perform();//可点击
+        driver.navigate().back();
+        driver.navigate().back();
+
+        touch.tap(PointOption.point(353,105)).perform();//可点击
+        Thread.sleep(10000);
+        driver.navigate().back();
+        driver.navigate().back();
+
+        Thread.sleep(10000);
+    }
+
+    @Test
+    //点击clickable=false的元素-》结果：可以点击，并跳转页面
+    public void testClickUnclickable() throws InterruptedException {
+        WebElement e=  driver.findElement(By.xpath("//*[@text='沪深' and @resource-id='com.xueqiu.android:id/button_text']"));
+        System.out.println("是否可被点击："+e.getAttribute("clickable"));
+        e.click();
+        Thread.sleep(10000);
+
     }
 
     @AfterAll
